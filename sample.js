25eedc4325e040e508692297b456e1bd0630b87d
sahithapex <t.sahithchandra@gmail.com>
2025-02-05 01:23:54 +0530

    fibonacci

---
+++ sample.js
@@ -0,0 +1,12 @@
+function fibonacci(n) {
+      if (n <= 1) return n;
+      return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+
+// Initialize with any number
+const num = 15;
+console.log(`Fibonacci sequence up to ${num}:`);
+for (let i = 0; i < num; i++) {
+      console.log(fibonacci(i));
+}
