d812b514692087a37d3640c19c9e39755ca14472
sahithapex <t.sahithchandra@gmail.com>
2025-03-05 23:30:10 +0530

    youtube clone

--- youtube clone/src/pages/video/Video.jsx
+++ youtube clone/src/pages/video/Video.jsx
@@ -1,12 +1,29 @@
-import React from 'react'
-import './video.css'
+import React, { useRef, useState } from 'react';
+import './video.css';
+import { Play, Pause } from 'lucide-react';
 
-const Video = () => {
+const Video = ({ src }) => {
+  const videoRef = useRef(null);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const togglePlay = () => {
+    if (videoRef.current.paused) {
+      videoRef.current.play();
+      setIsPlaying(true);
+    } else {
+      videoRef.current.pause();
+      setIsPlaying(false);
+    }
+  };
+
   return (
-    <div>
-      
+    <div className="video-container">
+      <video ref={videoRef} src={src} className="video-player" controls />
+      <button className="play-button" onClick={togglePlay}>
+        {isPlaying ? <Pause size={24} /> : <Play size={24} />}
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default Video
+export default Video;
