805d6eed3b57f0603ed33c685dd85e0f82a29c26
sahithapex <t.sahithchandra@gmail.com>
2025-03-13 20:48:58 +0530

    toggle

--- Personal Finance Dashboard/src/components/Toggle.jsx
+++ Personal Finance Dashboard/src/components/Toggle.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
-
 const Toggle = () => {
+  const [isOn, setIsOn] = useState(false);
+
+  const toggleSwitch = () => {
+    setIsOn(prevState => !prevState);
+  };
+
   return (
     <div>
-      
+      <span>{isOn ? 'On' : 'Off'}</span>
+      <button onClick={toggleSwitch}>
+        {isOn ? 'Turn Off' : 'Turn On'}
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default Toggle
+export default Toggle;
