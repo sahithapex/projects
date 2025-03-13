da4004b59738366aea02e92285cdd8076b510ceb
sahithapex <t.sahithchandra@gmail.com>
2025-03-14 00:33:57 +0530

    small changes

--- HabitStack/src/components/Habits/Addstack/Addstack.jsx
+++ HabitStack/src/components/Habits/Addstack/Addstack.jsx
@@ -18,8 +18,7 @@ const Addstack = () => {
     <div className="addstack-container">
       <h1 className="addstack-heading">Habit Stack</h1>
       <form className="addstack-form" onSubmit={handleSubmit}>
-        <label className="addstack-label">Habit Stack Title:</label>
-        <select className="addstack-select" name="routine">
+        <select className="addstack-input" name="routine">
           <option value="">Select Routine</option>
           {Routine.map((routine) => (
             <option key={routine.id} value={routine.label}>
@@ -29,43 +28,21 @@ const Addstack = () => {
         </select>
 
         <div className="addstack-input-group">
-          <label className="addstack-label">Routine Name:</label>
-          <input type="text" name="name" placeholder="Enter your habit stack" className="addstack-input" />
+        {[...Array(5)].map((_, index) => (
+          <div key={index} className="addstack-main">
+            <input
+              type="text"
+              name={`habit-${index + 1}`}
+              placeholder={`Habit number ${index + 1}`}
+              className="addstack-input"
+            />
+            <input type="time" className="addstack-time" required />
+          </div>
+        ))}
         </div>
 
-        <div className="addstack-time">
-          <label className="addstack-label">Select Time:</label>
-        </div>
         <div className="addstack-time-selection">
-          <div>
-            <label className="addstack-label">Hours:</label>
-            <select className="addstack-select" name="hours">
-              {[...Array(13)].map((_, i) => (
-                <option key={i} value={i.toString().padStart(2, "0")}>
-                  {i.toString().padStart(2, "0")}
-                </option>
-              ))}
-            </select>
-          </div>
-          <div>
-            <label className="addstack-label">Minutes:</label>
-            <select className="addstack-select" name="minute">
-              {[0, 15, 30, 45].map((min) => (
-                <option key={min} value={min.toString().padStart(2, "0")}>
-                  {min.toString().padStart(2, "0")}
-                </option>
-              ))}
-            </select>
-          </div>
-          <div>
-            <label className="addstack-label">AM/PM:</label>
-            <select className="addstack-select" name="ampm">
-              <option value="AM">AM</option>
-              <option value="PM">PM</option>
-            </select>
-          </div>
         </div>
-
         <button type="submit" className="sub-btn">Submit</button>
       </form>
     </div>
---
+++ HabitStack/src/components/Habits/Addstack/Finallist.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const Finallist = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Finallist
